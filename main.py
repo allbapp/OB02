670db4ff26e9c876c96bfa663b90310e73bcab13
allbapp <163859356+allbapp@users.noreply.github.com>
2024-04-01 18:48:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 class User():
     def __init__(self, id, name, access):
         self.id = id
-        self.name = name
-        self.__private_access = access  #приватный атрибут
+        self._name = name
+        self._access = access  #приватный атрибут
 
 
 
-    def get_private(self):
-        return self.__private_access
+    def get_access(self):
+        return self._access
+    def get_name(self):
+        return self._name
 
-    def set_private(self, value):
-        self.__private_access = value
 
-
+    def set_name(self, name):
+        self._name = name
 
 class Admin(User):
     def __init__(self, id, name, access, admin):
         super().__init__(id, name, access)
-        self.__private_admin = admin #приватный атрибут
+        self._admin = admin #приватный атрибут
 
-        def get_private(self):
-            return self.__private_admin
+    def add_user(self, user_list, name):
+        user_list.append(name)
+        print(f"Add user:")
+        print(user_list)
 
-        def set_private(self, value):
-            self.__private_admin = value
-
-    def add_user(self):
-        print(f"Add user: {self.name}")
-
-    def remove_user(self):
-        print(f"Remove user: {self.name}")
-
-    def test_private(self):
-        self.__private_add_user()
-
-    def test_private(self):
-        self.__private_remove_user()
+    def remove_user(self, user_list, name):
+        user_list.remove(name)
+        print(f"Remove user: ")
 
 
 
-user1 = User(1, "John", "user")
-user2 = User(2, "Jane", "user")
-user3 = Admin(3, "Bob", "user", "admin")
-user4 = User(4, "Kate", "user")
-user5 = Admin(5, "Alex", "user", "admin")
+users = []
+admin = Admin(1, "John", "user", "admin")
+user1 = User(2, "Jane", "user")
+user2 = User(3, "Jack", "user")
+user3 = User(4, "Jill", "user")
 
-
-
-user3.add_user()
-user5.remove_user()
-user5.add_user()
-
-print(user1.get_private())
-print(user2.get_private())
-print(user3.get_private())
-print(user4.get_private())
-print(user5.get_private())
-
-
+print(user1.get_name())
+admin.add_user(users, "John")
+admin.add_user(users, "Kate")
